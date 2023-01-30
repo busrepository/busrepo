9dfed0b30d3cc499d40bdd8ece0b6fc29b111f16
busrepository <119661846+busrepository@users.noreply.github.com>
2023-01-30 23:59:27 +0530

    Add files via upload

---
+++ news.js
@@ -0,0 +1,2 @@
+var news1= "Kolkata Book Fair special bus <a href='https://www.facebook.com/Kolbusopedia/photos/a.438176566330497/2415169575297843/?type=3' target='_blank'>Check here</a><br>New route: EB12, E76, E77, D34<br>S-111 Howrah LakeRoad Mini closed<br>Dankuni Kamalgazi back to old route, via Shyambazar Bonhoogly<br>";
+var news2= "<br>";
