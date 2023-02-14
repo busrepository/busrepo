190310215bf08c14831d3020eb3cf98f9002685c
busrepository <119661846+busrepository@users.noreply.github.com>
2023-02-14 22:30:37 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,2 +1,3 @@
-var news1= "Kolkata Book Fair special bus <a href='https://www.kolbusopedia.com/forum/questions-answers/buses-that-will-take-you-to-kolkata-international-book-fair' target='_blank'>Check here</a><br>New route: EB12, E76, E77, D34<br>S-111 Howrah LakeRoad Mini closed<br>Dankuni Kamalgazi back to old route, via Shyambazar Bonhoogly<br>";
+var news1= "New route:Howrah to Garh Bhowanipore via Amta<br>New route: EB12, E76, E77, D34<br>S-111 Howrah LakeRoad Mini closed<br>Dankuni Kamalgazi back to old route, via Shyambazar Bonhoogly<br>";
 var news2= "<br>";
+var news3= "<br>";
