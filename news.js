7de3ac9d80b6c504db82a104663c6bff72651254
busrepository <119661846+busrepository@users.noreply.github.com>
2023-04-01 12:03:09 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,3 +1,3 @@
 var news1= "44 route temporarily off due to roadworks<br>AC-47 route Re-introduced<br>New route:Howrah to Garh Bhowanipore via Amta<br>EB12 Extented upto Karunamoyee<br>JM2 route closed";
 var news2= "<br>";
-var news3= "New route:Kolkata to Shimuliyahat<br>";
+var news3= "New route:Kolkata to Shimuliyahat<br>Newtown to Digha AC Volvo Introduced";
