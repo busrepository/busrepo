7efc80eb732ce7a450141e66acfb849326314491
busrepository <119661846+busrepository@users.noreply.github.com>
2023-05-21 00:43:52 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,3 +1,3 @@
-var news1= "BE-7 Esplanade to Balurghat AC Volvo introduced<br>44 route temporarily off due to roadworks<br>AC-47 route Re-introduced";
+var news1= "ACT-1 spl. Esplanade to Siliguri AC Volvo introduced<br>BE-7 Esplanade to Balurghat AC Volvo introduced<br>44 route temporarily off due to roadworks<br>";
 var news2= "<br>";
 var news3= "New route:Kolkata to Shimuliyahat<br>Newtown to Digha AC Volvo Introduced";
