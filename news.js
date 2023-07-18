1052c9cd968b360e24fbca13ebe2e0e8016b2030
busrepository <119661846+busrepository@users.noreply.github.com>
2023-07-18 19:55:18 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,3 +1,3 @@
 var news1= "235 extended upto Phoolbagan";
-var news2= "<br>";
-var news3= "New route:Kolkata to Shimuliyahat<br>Newtown to Digha AC Volvo Introduced";
+var news2= "Kolkata to Dhuliyan NBSTC introduced";
+var news3= "<br>";
