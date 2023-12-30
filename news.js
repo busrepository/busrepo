ef1c9f65dd8512ac3d75fa53cff600878537be4c
busrepository <119661846+busrepository@users.noreply.github.com>
2023-12-30 12:59:43 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,3 +1,3 @@
-var news1= "New STA route: Ghatakpukur to Bagbazar<br>New WBTC Routes!!!<br>36: Khiddirpore to Esplanade<br>ACT15: Kolkata to Bakhhali<br>VS15, VS15A: Amtala to Dakshineshwar<br>D1A: Barasat Tollygunge";
+var news1= "New STA route: Ghatakpukur to Bagbazar, Danesh SK Lane to Shapoorji, Amta to Shapoorji<br>New WBTC Routes!!!<br>36: Khiddirpore to Esplanade<br>ACT15: Kolkata to Bakhhali<br>VS15, VS15A: Amtala to Dakshineshwar<br>D1A: Barasat Tollygunge";
 var news2= "Kolkata to Dhuliyan NBSTC introduced";
 var news3= "New route:Kolkata to Siliguri";
