b976a7b4848b7888fa759d180bc7919d82ef38b0
busrepository <119661846+busrepository@users.noreply.github.com>
2024-01-23 23:41:24 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,4 +1,4 @@
 //var news1= "New STA route: Ghatakpukur to Bagbazar, Danesh SK Lane to Shapoorji, Amta to Shapoorji<br>New WBTC Routes!!!<br>36: Khiddirpore to Esplanade<br>ACT15: Kolkata to Bakhhali<br>VS15, VS15A: Amtala to Dakshineshwar<br>D1A: Barasat Tollygunge";
-var news1="Bookfair special bus: Ultadanga(Ch9), Sealdah(Ch1), Santragachi (VS-12)(Ch2), BallyHalt(Ch11), Gariahat(Ch6), Tollygunje Metro(Ch3), Joka(Ch6), Jadavpur (Ch4,5), Barasat (Ch8), Baruipur(Ch7)";
+var news1="Bookfair special bus: Sealdah(Ch1), Howrah (Ch2), Tollygunje Metro(Ch3), Jadavpur (Ch4,5), Gariahat(Ch6), Joka(Ch6), Baruipur(Ch7), Santragachi (VS-12)(Ch8), Barasat (Ch8), Ultadanga(Ch9), Airport (Ch10), Barrackpore (ch10), BallyHalt(Ch11)";
 var news2= "Kolkata to Dhuliyan NBSTC introduced";
 var news3= "New route:Kolkata to Siliguri";
