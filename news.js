f7ebcca1a328efff59a79ef61ad68dd96a888006
busrepository <119661846+busrepository@users.noreply.github.com>
2024-02-23 20:07:42 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,4 +1,4 @@
 //var news1= "New STA route: Ghatakpukur to Bagbazar, Danesh SK Lane to Shapoorji, Amta to Shapoorji<br>New WBTC Routes!!!<br>36: Khiddirpore to Esplanade<br>ACT15: Kolkata to Bakhhali<br>VS15, VS15A: Amtala to Dakshineshwar<br>D1A: Barasat Tollygunge";
-var news1="S-117 Mini Kalitala Howrah mini closed<br>RT-31 Makardaha Khiddirpore route closed";
+var news1="S-117 Mini Kalitala Howrah mini closed<br>RT-31 Makardaha Khiddirpore route closed<br>Change in alignment of Danesh SK Shapoorji, check bus route";
 var news2= "Kolkata to Dhuliyan NBSTC introduced";
 var news3= "New route:Kolkata to Siliguri";
