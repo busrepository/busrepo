05067d06f7c883300ff5589cf8062b71112bba2c
busrepository <119661846+busrepository@users.noreply.github.com>
2024-04-18 18:21:22 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,4 +1,4 @@
 //var news1= "New STA route: Ghatakpukur to Bagbazar, Danesh SK Lane to Shapoorji, Amta to Shapoorji<br>New WBTC Routes!!!<br>36: Khiddirpore to Esplanade<br>ACT15: Kolkata to Bakhhali<br>VS15, VS15A: Amtala to Dakshineshwar<br>D1A: Barasat Tollygunge";
-var news1="235 terminating at Rajabazar<br>Change in alignment of Danesh SK Shapoorji, check bus route<br>New Route: Barasat to Alampur";
+var news1="New Route: Barasat to Alampur";
 var news2= "";
 var news3= "";
