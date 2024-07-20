47b199088d42be0621f1c6377479bcb740510e79
busrepository <119661846+busrepository@users.noreply.github.com>
2024-07-20 13:16:48 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,4 +1,4 @@
 //var news1= "New STA route: Ghatakpukur to Bagbazar, Danesh SK Lane to Shapoorji, Amta to Shapoorji<br>New WBTC Routes!!!<br>36: Khiddirpore to Esplanade<br>ACT15: Kolkata to Bakhhali<br>VS15, VS15A: Amtala to Dakshineshwar<br>D1A: Barasat Tollygunge";
-var news1="DN2 route is closed<br>New Route: Barasat to Alampur via Dunlop<br> Ghatakpukur to Bagbazar extended upto Rajchandrapur";
+var news1="DN2 route is closed<br> DN17 Extended upto Santragachi<br>New Route: Barasat to Alampur via Dunlop";
 var news2= "";
 var news3= "";
