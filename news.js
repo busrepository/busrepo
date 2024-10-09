9cec460c22874baef256d4ec188b865c9728e9ff
busrepository <119661846+busrepository@users.noreply.github.com>
2024-10-10 00:00:59 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,3 +1,3 @@
-var news1="S61 BBD Bag Achipur restarted<br>New Routes EB13, EB16, DS1, C20, 211 G.B., Usthi Newtown, Shalimar Newtown";
+var news1="S61 BBD Bag Achipur restarted<br>New Routes EB13, EB16, DS1, C20, C42, 211 G.B., Usthi Newtown, Shalimar Newtown";
 var news2= "";
 var news3= "";
