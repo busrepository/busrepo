b9748a35dc65b1c4b194b3fdda947449e7ce4b6e
busrepository <119661846+busrepository@users.noreply.github.com>
2024-10-09 23:55:27 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,4 +1,3 @@
-//var news1= "New STA route: Ghatakpukur to Bagbazar, Danesh SK Lane to Shapoorji, Amta to Shapoorji<br>New WBTC Routes!!!<br>36: Khiddirpore to Esplanade<br>ACT15: Kolkata to Bakhhali<br>VS15, VS15A: Amtala to Dakshineshwar<br>D1A: Barasat Tollygunge";
-var news1="<br>S61 BBD Bag Achipur restarted<br>New Routes EB13, EB16, Usthi Newtown, Shalimar Newtown<br> DN17 Extended upto Santragachi<br>DN16/1 going via Saltlake";
+var news1="S61 BBD Bag Achipur restarted<br>New Routes EB13, EB16, DS1, C20, 211 G.B., Usthi Newtown, Shalimar Newtown";
 var news2= "";
 var news3= "";
