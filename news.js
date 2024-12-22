f614feecab4f9f8f2b8e98ee480448b96f4839f6
busrepository <119661846+busrepository@users.noreply.github.com>
2024-12-22 16:28:14 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,3 +1,3 @@
-var news1="New Routes D10, S-128/1, EB13, EB16, D35, DS1, C20, C42, 211 G.B., Shalimar Newtown";
+var news1="New Routes D10, S-128/1, EB13, EB16, D35, DS1, C20, C42, 211 G.B., Shalimar Newtown<br>SD33,SD63,SD64 restarting from Baruipur";
 var news2= "";
 var news3= "";
