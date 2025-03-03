e33ac983c5d969242cdf6f61700cc4574d11eba3
busrepository <119661846+busrepository@users.noreply.github.com>
2025-03-03 22:31:16 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,3 +1,3 @@
-var news1="International Book Fair Buses <a href='https://www.facebook.com/photo?fbid=937054141896677&set=a.546003304335098' target='_blank'>Check here</a><br> Few DN-9/1 buses realigned via Dumdum Stn.";
+var news1= "New Route KB24<br>KB21 starting from Bantala";
 var news2= "";
 var news3= "";
