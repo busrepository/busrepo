eb25eb5404e30a751234601ed940f9bf572aff20
busrepository <119661846+busrepository@users.noreply.github.com>
2025-03-05 00:32:57 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,3 +1,3 @@
-var news1= "New Route KB24 Hatishala Kolkata Stn.<br>KB21 starting from Leather Complex";
+var news1= "New Route KB24 Hatishala Kolkata Stn.<br>KB21 starting from Leather Complex<br>78/1 extented upto Mohishpota";
 var news2= "";
 var news3= "";
